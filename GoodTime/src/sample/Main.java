3deb6d1907bfe19da4241a3bb568df0fd5d057bf
MerenovaAnastasiya <31702117+MerenovaAnastasiya@users.noreply.github.com>
2017-11-23 10:20:48 +0300

    Update Main.java
    подключение стилей

--- GoodTime/src/sample/Main.java
+++ GoodTime/src/sample/Main.java
@@ -9,14 +9,17 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root);
+        
+        scene.getStylesheets().add("styles/style.css");
         primaryStage.setScene(scene);
         primaryStage.setTitle("Your Time");
         primaryStage.getIcons().add(new Image("/img/Icon.jpg"));
+        primaryStage.setMaxHeight(400);
+        primaryStage.setMaxWidth(600);
         primaryStage.show();
     }
 
