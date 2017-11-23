1bba37e4b12c81bd8a4a07c83f99cb600ec21d43
Qokr <kuroitenshi96@gmail.com>
2017-11-23 12:33:52 +0300

    Вывод секунд

--- GoodTime/src/entity/storeOfDays.java
+++ GoodTime/src/entity/storeOfDays.java
@@ -6,14 +6,15 @@ import java.util.ArrayList;
 public class storeOfDays extends ArrayList<Day> {
     public Day mostUsefulDay() {
         Day day = null;
-        while (this.size()>0){
+        while (this.size() > 0) {
 
         }
         return day;
     }
-    public Day mostUselessDay(){
+
+    public Day mostUselessDay() {
         Day day = null;
-        while (this.size()>0){
+        while (this.size() > 0) {
 
         }
         return day;
