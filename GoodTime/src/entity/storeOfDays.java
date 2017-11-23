df30cb29707dbfb1808526e03d449b24d9b6563d
Qokr <kuroitenshi96@gmail.com>
2017-11-23 10:38:03 +0300

    some changes

---
+++ GoodTime/src/entity/storeOfDays.java
@@ -0,0 +1,21 @@
+package entity;
+
+
+import java.util.ArrayList;
+
+public class storeOfDays extends ArrayList<Day> {
+    public Day mostUsefulDay() {
+        Day day = null;
+        while (this.size()>0){
+
+        }
+        return day;
+    }
+    public Day mostUselessDay(){
+        Day day = null;
+        while (this.size()>0){
+
+        }
+        return day;
+    }
+}
